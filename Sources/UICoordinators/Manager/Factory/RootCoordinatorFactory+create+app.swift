95c8a4cb9b4f1be9ca292fb547236eb5ec7b6f40
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-05-25 17:17:34 +0200

    Add possibility to create app RootCoordinator from Factory

---
+++ Sources/UICoordinators/Manager/Factory/RootCoordinatorFactory+create+app.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Łukasz Osiennik on 25/05/2021.
+//  Copyright © 2020 open plainness (https://www.openplainness.com). All rights reserved.
+//
+
+extension RootCoordinatorFactory {
+    
+    public static func create<DevRootWindowType>(createViewController: @autoclosure @escaping WindowCreator<DevRootWindowType>.ViewController.Create) -> RootCoordinator<DevRootWindowType> where DevRootWindowType: DevRootWindowTypeInterface {
+        return create(windowCreator: WindowCreator(entity: WindowEntity(type: .app), createViewController: createViewController()))
+    }
+    
+    public static func create<DevRootWindowType>(createWindow: @escaping WindowCreator<DevRootWindowType>.Window.Create, createViewController: @autoclosure @escaping WindowCreator<DevRootWindowType>.ViewController.Create) -> RootCoordinator<DevRootWindowType> where DevRootWindowType: DevRootWindowTypeInterface {
+        return create(windowCreator: WindowCreator(entity: WindowEntity(type: .app), createWindow: createWindow, createViewController: createViewController()))
+    }
+}
