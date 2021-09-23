3c56c278fe1df7ef56e5ca09b1bbd31050461867
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-09-23 16:18:24 +0200

    develop code

--- Package.swift
+++ Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "UICoordinators",
+    name: "AppNavigation",
     platforms: [
         .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "UICoordinators",
+            name: "AppNavigation",
             targets: [
-                "UICoordinators",
+                "AppNavigation",
             ]
         ),
     ],
@@ -25,7 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "UICoordinators",
+            name: "AppNavigation",
             dependencies: []
         ),
     ]
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# UICoordinators
+# AppNavigation
 
 A description of this package.
